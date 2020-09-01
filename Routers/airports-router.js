aa8b32b3bb79f61d23124ff9335c8e75447adc76
salakjit permsuklarp <salakjit.psl@gmail.com>
2020-09-01 10:34:34 +0700

    update router airport

--- Routers/airports-router.js
+++ Routers/airports-router.js
@@ -4,15 +4,15 @@ const  airport_controller = require("../Controllers/airport-controller");
 
 //GET
 router.get('/', airport_controller.getList);
-// router.post("/import",airport_controller.importJSON);
+router.post("/import",airport_controller.importJSON);
 
-// //POST
-// router.post("/", airport_controller.insertAirport);
+//POST
+router.post("/", airport_controller.insertAirport);
 
-// //PUT
-// router.put('/:id', airport_controller.updateAirport);
+//PUT
+router.put('/:id', airport_controller.updateAirport);
 
-// //DEL
-// router.delete('/:id', airport_controller.deleteAirport);
+//DEL
+router.delete('/:id', airport_controller.deleteAirport);
 
 module.exports = router;
