33201a8009e5d80074e58cfef3e5d00e6fd48fc8
salakjit permsuklarp <salakjit.psl@gmail.com>
2020-09-02 12:41:07 +0700

    add pagination

--- Controllers/airport-controller.js
+++ Controllers/airport-controller.js
@@ -1,28 +1,83 @@
 const Airports = require('../Models/airports-model');
+const pagination = require('../Helpers/pagination/pagination');
 
-exports.getList = function (req, res) {
-    const id = (req.params && req.params.id) ? req.params.id : (req.query && req.query.id) ? req.query.id : null;
-    const typeId = (req.params && req.params.typeId) ? req.params.typeId : (req.query && req.query.typeId) ? req.query.typeId : null;
+exports.getList = async function (req, res){
     
-    if(typeId && typeId > 0){
-        Airports.find({typeId : typeId}).exec((err, data) => {
-            if (err) return res.status(400).send(err);
-            res.status(200).send(data);
-        });
+    try{
+        const page = parseInt((req.params && req.params.page) ? req.params.page : (req.query && req.query.page) ? req.query.page : 1);
+        const size = parseInt((req.params && req.params.size) ? req.params.size : (req.query && req.query.size) ? req.query.size : 10);
+
+        const id = (req.params && req.params.id) ? req.params.id : (req.query && req.query.id) ? req.query.id : null;
+        const typeId = (req.params && req.params.typeId) ? req.params.typeId : (req.query && req.query.typeId) ? req.query.typeId : null;
+
+        let [_results, _count] = [];
+
+        if(id){
+            [_results, _count] = await Promise.all([
+                Airports.findById(id)
+                //   .skip(pagination.calSkip(page, size))
+                //   .limit(size)
+                  .exec(),
+                  Airports.countDocuments().exec()
+                ]);
+        }
+        else if(typeId && typeId > 0){
+            [_results, _count] = await Promise.all([
+                Airports.find({typeId : typeId})
+                //   .skip(pagination.calSkip(page, size))
+                //   .limit(size)
+                  .exec(),
+                  Airports.countDocuments().exec()
+                ]);
+        } 
+        else{
+            [_results, _count] = await Promise.all([
+                Airports.find()
+                //   .skip(pagination.calSkip(page, size))
+                //   .limit(size)
+                  .exec(),
+                  Airports.countDocuments().exec()
+                ]);
+        }
+
+        const resValue = {
+            currentPage: page,
+            allPages: pagination.calPage(_count, size),
+            currentCount: _results ? _results.length : 0,
+            totalCount: _count,
+            data: _results
+        }
+
+        res.status(200).send(resValue);
     }
-    else if(id){
-        Airports.findById(id, function (err, data) {
-            if (err) return res.status(400).send(err);
-            res.status(200).send(data);
-        })
+    catch(err){
+        res.status(400).send(err);
     }
-    else{
-        Airports.find().exec((err, data) => {
-            if (err) return res.status(400).send(err);
-            res.status(200).send(data);
-        });
-    }
-}
+};
+
+// exports.getList = function (req, res) {
+//     const id = (req.params && req.params.id) ? req.params.id : (req.query && req.query.id) ? req.query.id : null;
+//     const typeId = (req.params && req.params.typeId) ? req.params.typeId : (req.query && req.query.typeId) ? req.query.typeId : null;
+
+//     if(typeId && typeId > 0){
+//         Airports.find({typeId : typeId}).exec((err, data) => {
+//             if (err) return res.status(400).send(err);
+//             res.status(200).send(data);
+//         });
+//     }
+//     else if(id){
+//         Airports.findById(id, function (err, data) {
+//             if (err) return res.status(400).send(err);
+//             res.status(200).send(data);
+//         })
+//     }
+//     else{
+//         Airports.find().exec((err, data) => {
+//             if (err) return res.status(400).send(err);
+//             res.status(200).send(data);
+//         });
+//     }
+// }
 
 const data = require('../importAirports.json')
 exports.importJSON = (req, res) => {
